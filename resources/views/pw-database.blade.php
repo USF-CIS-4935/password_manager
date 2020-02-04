a814f26d40a2d8753aedff95fc88eee2c309fcd2
Ryan <ryanborum@gmail.com>
2020-02-03 20:21:02 -0500

    Major layout and styling changes to database page

--- resources/views/pw-database.blade.php
+++ resources/views/pw-database.blade.php
@@ -1,67 +1,50 @@
 @include('templates/head-tag', ['title' => 'Password Database'])
 
 <body>
-@include('templates/left-nav')
-<div class="container">
-  <p class="header-text">Password Database</p>
-  <div class="top-search">
-    <i class="fas fa-search icon"></i>
-    <input id="db-search" class="text-search" type="text" placeholder="Search By Password Name">
-  </div>
-  <div class="password-panels">
-    <div class="pw-panel">
-      <div class="panel-top">
-      </div>
-      <div class="panel-bottom">
-        <h3 class="panel-title">Password Name</h3>
-        <p class="subtitle">Username Here</p>
-        <p class="subtitle">Password expires in: 10 days</p>
-        <p class="panel-links"><i class="fas fa-copy"></i><i class="fas fa-edit"></i></p>
-      </div>
+  @include('templates/left-nav')
+  <div class="container">
+    <p class="header-text">Password Database</p>
+    <div class="top-search">
+      <i class="fas fa-search icon"></i>
+      <input id="db-search" class="text-search" type="text" placeholder="Search By Password Name">
     </div>
-    <div class="pw-panel">
-      <div class="panel-top">
+    <div class="password-panels">
+      <div class="pw-panel">
+        <div class="date-field">
+          <span class="day-counter">14 </span>days
+        </div>
+        <div class="panel-right">
+          <h3 class="panel-title">Password Name</h3>
+          <p class="subtitle">Username Here</p>
+          <p class="subtitle">Password expires in: 10 days</p>
+        </div>
+        <p class="panel-links"><i class="fas fa-copy"></i></p>
       </div>
-      <div class="panel-bottom">
-        <h3 class="panel-title">Password Name</h3>
-        <p class="subtitle">Username Here</p>
-        <p class="subtitle">Password expires in: 10 days</p>
-        <p class="panel-links"><i class="fas fa-copy"></i><i class="fas fa-edit"></i></p>
+      <div class="pw-panel">
+        <div class="date-field">
+          <span class="day-counter">14 </span>days
+        </div>
+        <div class="panel-right">
+          <h3 class="panel-title">Password Name</h3>
+          <p class="subtitle">Username Here</p>
+          <p class="subtitle">Password expires in: 10 days</p>
+        </div>
+        <p class="panel-links"><i class="fas fa-copy"></i></p>
       </div>
-    </div>
-    <div class="pw-panel">
-      <div class="panel-top">
-      </div>
-      <div class="panel-bottom">
-        <h3 class="panel-title">Password Name</h3>
-        <p class="subtitle">Username Here</p>
-        <p class="subtitle">Password expires in: 10 days</p>
-        <p class="panel-links"><i class="fas fa-copy"></i><i class="fas fa-edit"></i></p>
-      </div>
-    </div>
-    <div class="pw-panel">
-      <div class="panel-top">
-      </div>
-      <div class="panel-bottom">
-        <h3 class="panel-title">Password Name</h3>
-        <p class="subtitle">Username Here</p>
-        <p class="subtitle">Password expires in: 10 days</p>
-        <p class="panel-links"><i class="fas fa-copy"></i><i class="fas fa-edit"></i></p>
-      </div>
-    </div>
-    <div class="pw-panel">
-      <div class="panel-top">
-      </div>
-      <div class="panel-bottom">
-        <h3 class="panel-title">Password Name</h3>
-        <p class="subtitle">Username Here</p>
-        <p class="subtitle">Password expires in: 10 days</p>
-        <p class="panel-links"><i class="fas fa-copy"></i><i class="fas fa-edit"></i></p>
+      <div class="pw-panel">
+        <div class="date-field">
+          <span class="day-counter">14 </span>days
+        </div>
+        <div class="panel-right">
+          <h3 class="panel-title">Password Name</h3>
+          <p class="subtitle">Username Here</p>
+          <p class="subtitle">Password expires in: 10 days</p>
+        </div>
+        <p class="panel-links"><i class="fas fa-copy"></i></p>
       </div>
     </div>
   </div>
-</div>
-<!-- <script
+  <!-- <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
   crossorigin="anonymous">
