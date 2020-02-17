310d02d90b1ea057c110e0c27f18cb4e56959dcd
ryanborum <ryanborum@gmail.com>
2020-02-17 10:26:31 -0500

    Modified user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('master_password');
+            $table->string('master_salt');
             $table->timestamps();
         });
     }
