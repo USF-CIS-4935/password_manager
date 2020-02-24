bbb789e15c3bf5dfe9e3f4b34603006c2751872c
Ryan <ryanborum@gmail.com>
2020-02-24 18:55:22 -0500

    Consolidated migrations

--- database/migrations/2020_02_20_001242_create_account_options_table.php
+++ database/migrations/2020_02_20_001242_create_account_options_table.php
@@ -15,6 +15,8 @@ class CreateAccountOptionsTable extends Migration
     {
         Schema::create('account_options', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->boolean('password_age_notification');
             $table->boolean('failure_lockout_timer');
             $table->timestamps();
