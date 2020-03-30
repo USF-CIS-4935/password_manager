7aa98af1cb1735a8d0209cb9867a4d794ca45ccf
sameerenaik <sameer.naik19@gmail.com>
2020-03-30 07:34:28 -0400

    citing Project Nayuki for password generation code

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -4,6 +4,7 @@
   @include('templates/left-nav')
   <div class="container">
     <p class="header-text">Help and Information</p>
+    <p> Password Generation Code written by Project Nayuki: https://www.nayuki.io/page/random-password-generator-javascript</p>
     </div>
     <!-- <script
     src="https://code.jquery.com/jquery-3.4.1.min.js"
