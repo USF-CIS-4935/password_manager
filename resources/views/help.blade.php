baf6e3fd95df16a50ea54836947817d175fe5448
ryanborum <ryanborum@gmail.com>
2020-04-28 23:20:35 -0400

    Added small animation to increase notification visibility. Addition of major dependencies to "help" page

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -7,17 +7,30 @@
     <p>
       <strong>Password Generation (Project Nayuki)</strong>
       <br>
-      <a href="https://www.nayuki.io/page/random-password-generator-javascript" target="_blank">https://www.nayuki.io/page/random-password-generator-javascript</a>
+      <a href="https://www.nayuki.io/page/random-password-generator-javascript" target="_blank">nayuki.io/page/random-password-generator-javascript</a>
     </p>
     <p>
       <strong>CryptoJS 3.1.2:</strong>
       <br>
-      <a href="https://code.google.com/archive/p/crypto-js/" target="_blank">https://code.google.com/archive/p/crypto-js/</a>
+      <a href="https://code.google.com/archive/p/crypto-js/" target="_blank">code.google.com/archive/p/crypto-js/</a>
     </p>
     <p>
       <strong>Authentication Page Background Pattern:</strong>
       <br>
       <a href="https://indexstudio.net/" target="_blank"> "Shiva"</a>
+    </p>
+
+    <hr>
+
+    <p>
+      <strong>Laravel 6.2</strong>
+      <br>
+      <a href="https://laravel.com/docs/6.x" target="_blank">laravel.com</a>
+    </p>
+    <p>
+      <strong>JQuery 3.1.2</strong>
+      <br>
+      <a href="https://jquery.com/download/" target="_blank">JQuery.com</a>
     </p>
   </div>
 </body>
